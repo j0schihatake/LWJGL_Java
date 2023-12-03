b2697a8b042e48c14c34aa1847e281e61eca0d48
j0schitadamori <j0schitadamori@gmail.com>
2023-12-03 13:54:50 +0300

    Попробовал запустить окно.

--- src/main/java/org/j0schi/Main.java
+++ src/main/java/org/j0schi/Main.java
@@ -1,8 +1,21 @@
 package org.j0schi;
 
+import org.j0schi.core.WindowManager;
+import org.lwjgl.Version;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println(Version.getVersion());
+
+        WindowManager window = new WindowManager("DEV J0SCHI ENGINE", 1600, 900, false);
+        window.init();
+
+        while (!window.windowShouldClose()) {
+            window.update();
+        }
+
+        window.cleanup();
     }
 }
