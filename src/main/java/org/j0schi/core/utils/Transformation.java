d26e66a3e916598355f95d0cf576c1a43eeb7e37
j0schitadamori <j0schitadamori@gmail.com>
2023-12-05 12:59:00 +0300

    Урок 9 перемещение камеры отностиельно кубика.

--- src/main/java/org/j0schi/core/utils/Transformation.java
+++ src/main/java/org/j0schi/core/utils/Transformation.java
@@ -1,8 +1,10 @@
 package org.j0schi.core.utils;
 
+import org.j0schi.core.Camera;
 import org.j0schi.core.entity.Entity;
 import org.joml.Math;
 import org.joml.Matrix4f;
+import org.joml.Vector3f;
 
 public class Transformation {
 
@@ -13,6 +15,18 @@ public class Transformation {
                 .rotateY((float) Math.toRadians(entity.getRotation().y))
                 .rotateZ((float) Math.toRadians(entity.getRotation().z))
                 .scale(entity.getScale());
+        return matrix;
+    }
+
+    public static Matrix4f getViewMatrix(Camera camera){
+        Vector3f pos = camera.getPosition();
+        Vector3f rot = camera.getRotation();
+        Matrix4f matrix = new Matrix4f();
+        matrix.identity();
+        matrix.rotate((float) Math.toRadians(rot.x), new Vector3f(1,0,0))
+                .rotate((float) Math.toRadians(rot.y), new Vector3f(0,1,0))
+                .rotate((float) Math.toRadians(rot.z), new Vector3f(0,0,1));
+        matrix.translate(-pos.x, -pos.y, -pos.z);
         return matrix;
     }
 }
